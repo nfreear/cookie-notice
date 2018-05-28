77a7061efaebaa8d13027198c6127c88ca5e568c
Nick Freear <nfreear@yahoo.co.uk>
2018-05-28 22:42:45 +0100

    Add accessibility test with "rsids_pa11y" to Gruntfile / Travis-CI
    * Standard: WCAG 2.0 AA;
    * See ~ http://pa11y.org/

--- gruntfile.js
+++ gruntfile.js
@@ -20,6 +20,19 @@ module.exports = function (grunt) {
                 }
             }
         },
+        rsids_pa11y: { // Accessibility test ~ see http://pa11y.org/
+            accessibility: {
+                options: { // Task-specific options go here.
+                    standard: 'WCAG2AA',
+                    timeout: 8000,
+                    wait: 1000
+                },
+                url: [
+                    'http://localhost:8000/tests/pa11y.html'
+                ]
+                //, file: [ 'array of files, globbing permitted' ]
+            }
+        },
         strip_code: {
 
             src: {
@@ -45,8 +58,11 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-strip-code');
+    grunt.loadNpmTasks('grunt-rsids-pa11y');
 
     // Default task(s).
     grunt.registerTask('default', ['connect', 'qunit', 'strip_code', 'uglify']);
+
+    grunt.registerTask('pa11y', [ 'connect', 'rsids_pa11y' ]);
 
 };
